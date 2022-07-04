f0fff2cd1f28b82bc3ec1c696744e3a5bb7680f0
HamidReza <delshad.swdn@gmail.com>
2022-07-04 16:45:55 +0200

    create a register form and put its tag in App.jsx

---
+++ src/Register.jsx
@@ -0,0 +1,12 @@
+const Register = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your email" />
+      <input type="text" placeholder="Enter your password" />
+      <input type="text" placeholder="Repeat your password" />
+      <botton>Register</botton>
+    </div>
+  );
+};
+
+export default Register;
