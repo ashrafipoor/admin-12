bc8ff4f40df31a7473ddb3cd7221b120b467f34d
Afsaneh <jahanpour.aj@gmail.com>
2022-07-04 21:29:39 -0700

    add login file in src

---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Login(){
+    return(
+        <div>
+            <label>
+                Enter Name
+            </label>
+            <input id="name" type="text" />
+            <button>submit</button>
+        </div>
+    );
+}
+
+
+export default Login;
