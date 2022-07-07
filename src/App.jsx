7b70402fff2175af7c497db4a3aa1a0ced58c54d
Ramin <sahelram@gmail.com>
2022-07-08 02:09:02 +0430

    add content dashboard and more info and sales and visitores

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Content_dashboard from './Components/Content/Content-Dashboard'
+import Content_more from './Components/Content/Content-more'
+import Content_sales from './Components/Content/Content-sales'
+import Content_visitors from './Components/Content/Content-visitors'
 function App() {
 
   return (
     <main className="flex" >
-      <aside className="w-1/6 bg-slate-200 h-screen">
-
+      <aside className="w-1/6 bg-slate-500 h-screen">
       </aside>
       <section className="w-5/6 bg-orange-300 h-screen">
       <Header/>
-      <section></section>
-      <Footer/>
+      <section className="bg-slate-100 h-screen w-full ">
+          <Content_dashboard/>
+          <Content_more/>
+          <Content_sales/>
+          <Content_visitors/>
+           </section>
+          <Footer/>
       </section>
-     
-    </main>
-  )
+        </main>
+    )
 }
 
 export default App
